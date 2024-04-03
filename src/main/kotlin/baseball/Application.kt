6231df07b0d6d64f3773b2f1fb5ee24eb546a89b
NamGung Hyemin <ng040115@gmail.com>
2024-04-03 22:11:22 +0900

    🎨 게임에 대한 결과를 반환하는 로직 구현
    input 로직을 세분화

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,121 +1,110 @@
 package baseball
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.collections.mutableListOf as mutableListOf
 import camp.nextstep.edu.missionutils.Console
+import java.lang.Error
+import java.util.Collections
 
-const val NUMBER_OF_DIGITS = 3
-
-const val WORD_OF_RESTART = "1"
-const val WORD_OF_END = "2"
-const val WORD_OF_BALL = "볼"
-const val WORD_OF_STRIKE = "스트라이크"
-const val WORD_OF_NOTHING = "낫싱"
-
-const val NUMBER_OF_BALL = "numberOfBall"
-const val NUMBER_OF_STRIKE = "numberOfStrike"
-
-const val MESSAGE_OF_START = "숫자 야구 게임을 시작합니다"
-const val MESSAGE_OF_INPUT  = "숫자를 입력해주세요 : "
-const val MESSAGE_OF_ASK_RESTART = "게임을 새로 시작하려면 ${WORD_OF_RESTART}, 종료하려면 ${WORD_OF_END}를 입력하세요."
-const val MESSAGE_OF_WIN = "${NUMBER_OF_DIGITS}개의 숫자를 모두 맞히셨습니다! 게임 종료"
-const val MESSAGE_OF_END = "게임을 종료합니다."
-
+const val INTRO_MESSAGE = "숫자 야구 게임을 시작합니다"
+const val NOTHING_MESSAGE = "낫싱"
+const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
+const val RETRY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+const val SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! "
+const val END_MESSAGE = "게임 종료"
+const val COMPUTER_RANDOM_SIZE = 3
 fun main() {
-    var isPlaying = true
-    println(MESSAGE_OF_START)
-
-    while (isPlaying) {
-        playGame()
-        isPlaying = askForRestart()
-    }
-
-    println(MESSAGE_OF_END)
-}
-
-fun playGame() {
-    val computerNumberList = makeRandomNumbersOfList()
-
-    var result:MutableMap<String, Int> = mutableMapOf(NUMBER_OF_BALL to 0, NUMBER_OF_STRIKE to 0)
-
-    while (result[NUMBER_OF_STRIKE] != NUMBER_OF_DIGITS) {
-        print(MESSAGE_OF_INPUT)
-        val playerNumberList = Console.readLine()?.let { makePlayerNumbersOfList(it) }
-
-        if (playerNumberList != null) {
-            result = getResult(computerNumberList, playerNumberList)
-        }
-        printResult(result)
-    }
+    println(INTRO_MESSAGE)
+    val computer = mutableListOf<String>()
+     input(computer)
 
 }
 
-fun makeRandomNumbersOfList(): MutableList<Int> {
-    val computerNumberList = mutableListOf<Int>()
-    while (computerNumberList.size < NUMBER_OF_DIGITS) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computerNumberList.contains(randomNumber)) {
-            computerNumberList.add(randomNumber)
+/**
+ * 컴퓨터의 결과값과 사용자의 입출력 받기
+ * */
+fun input(computer: MutableList<String>) :Unit{
+    print(INPUT_MESSAGE)   // 게임 시작
+    println(computer)
+    if(computer.isEmpty()){
+        while (computer.size < COMPUTER_RANDOM_SIZE) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9).toString()
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
         }
     }
-    return computerNumberList
+    print(computer)
+    val request:String = Console.readLine()
+    val user = request.map{i -> i.toString()}
+    print(user is MutableList)
+    validation(user)
+    checkBaseballGameResult(computer, user)
+
+
+
+}
+// 게임에 대한 결과를 반환하는 메소드
+fun checkBaseballGameResult(computer: MutableList<String>, user:List<String> ) {
+        val result: Pair<Int, Int> = compare(computer, user)
+        if(result ==Pair(0,0)){
+            println(NOTHING_MESSAGE)
+            input(computer)
+        }
+        if(result ==Pair(0,3)){
+            println("${result.second}스트라이크")
+            print(SUCCESS_MESSAGE)
+            println(END_MESSAGE)
+            println(RETRY_MESSAGE)
+            val request: String = Console.readLine()
+            try {
+                if (request == "1"){
+                   computer.remove("")
+                    input(computer)
+                }
+                if (request == "2"){
+                    return
+                }
+            }catch (e: Error){
+                throw  IllegalArgumentException()
+            }
+        }
+       else {
+           if(result.first ==0){
+               println("${result.second}스트라이크")
+           }
+            if (result.second==0){
+                println("${result.first}볼")
+            }
+            else {
+                println("${result.first}볼 ${result.second}스트라이크")
+
+            }
+            input(computer)
+        }
 }
 
-fun makePlayerNumbersOfList(input: String):MutableList<Int> {
-    checkExceptionOnInput(input)
+/**
+ * 두 배열을 받아서 볼과 스트라이크 개수 반환
+ */
+fun compare(computer: MutableList<String>, user: List<String>): Pair<Int, Int>{
+    var countStrike = 0
+    var countBall = 0
 
-    val userNumbers = mutableListOf<Int>()
-    for (i in 0 until NUMBER_OF_DIGITS) {
-        userNumbers.add(input[i].digitToInt())
+    for (i in user.indices) {
+        // i가 컴퓨터 배열 중 존재한다면
+        val res:Boolean = computer.contains(user[i])
+        if (res == true) {
+            // 근데 위치까지 같다면
+            if (user[i] == computer[i]) {
+                countStrike++
+            }
+            // 위치는 다르다면
+            else {
+                countBall++
+            }
+        }
     }
 
-    return userNumbers
+    return Pair( countBall,countStrike)
 }
 
-fun getResult(
-    computerNumberList: MutableList<Int>,
-    userNumberList: MutableList<Int>
-): MutableMap<String, Int> {
-
-    val numberOfStrike: Int =
-        userNumberList.count { computerNumberList.indexOf(it) == userNumberList.indexOf(it)}
-
-    val numberOfBall: Int =
-        userNumberList.count { computerNumberList.contains(it) } - numberOfStrike
-
-
-    val result = mutableMapOf(NUMBER_OF_BALL to numberOfBall, NUMBER_OF_STRIKE to numberOfStrike)
-    return result
-}
-
-fun printResult(result: MutableMap<String, Int>) {
-    if (result[NUMBER_OF_STRIKE] == NUMBER_OF_DIGITS) {
-        println("${NUMBER_OF_DIGITS}${WORD_OF_STRIKE}")
-        println(MESSAGE_OF_WIN)
-    }
-
-    if (result[NUMBER_OF_BALL] != 0) {
-        print("${result[NUMBER_OF_BALL]}$WORD_OF_BALL ")
-    }
-
-    if (result[NUMBER_OF_STRIKE] != 0) {
-        print("${result[NUMBER_OF_STRIKE]}$WORD_OF_STRIKE")
-    }
-
-    if (result[NUMBER_OF_STRIKE] == 0 && result[NUMBER_OF_BALL] == 0) {
-        print(WORD_OF_NOTHING)
-    }
-    println()
-}
-
-fun askForRestart(): Boolean {
-    println(MESSAGE_OF_ASK_RESTART)
-    val choice = Console.readLine()
-    if (choice == WORD_OF_RESTART) {
-        return true
-    }
-    if (choice == WORD_OF_END) {
-       return false
-    }
-    else {
-        throw IllegalArgumentException("${WORD_OF_RESTART}이나 $WORD_OF_END 외에 다른 것을 입력하셨습니다.")
-    }
-}
--- src/main/kotlin/baseball/Validation.kt
+++ src/main/kotlin/baseball/Validation.kt
@@ -1,29 +1,27 @@
 package baseball
 
-fun checkExceptionOnInput(input: String) {
-    isInteger(input)
-    isContainsDuplicated(input)
-    isCorrectNumberOfDigit(input)
-}
 
-fun isInteger(input: String) {
-    try{
-        input.toInt()
-    } catch (e: Exception) {
-        throw IllegalArgumentException("숫자를 입력해주세요.")
-    }
-}
 
-fun isContainsDuplicated(input: String) {
-    for (i in input) {
-       if (input.count { it == i } > 1) {
-           throw IllegalArgumentException("중복된 숫자를 입력하였습니다.")
-       }
-    }
-}
 
-fun isCorrectNumberOfDigit(input: String) {
-    if (input.length != NUMBER_OF_DIGITS) {
-        throw IllegalArgumentException("규칙에 맞지 않는 길이의 숫자를 입력하였습니다.")
+/* 예외 처리에 대한 함수 */
+fun validation(list: List<String>):Unit{
+    if(list.isEmpty())
+        throw IllegalArgumentException()
+    else if(list.size !=3)
+        throw IllegalArgumentException()
+    else if(list.distinct() != list)
+        throw IllegalArgumentException()
+    try {
+        for (i in 0..2){
+            if(list[i].toInt()<0){
+                throw IllegalArgumentException()
+            }
+        }
+
+    }catch (e :NumberFormatException){
+        throw IllegalArgumentException()
     }
+
+
+
 }
